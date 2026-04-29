93f0a1a4676ef24d69fc3fa5c359ab09957f6349
Prangon Rodi <rodiprangon@gmail.com>
2026-04-29 20:44:10 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '..

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -58,8 +58,8 @@ import {
   RefreshCw,
 } from 'lucide-react';
 import { Skeleton } from '@/components/ui/skeleton';
-import { Badge } from '../ui/badge';
-import { Checkbox } from '../ui/checkbox';
+import { Badge } from '@/components/ui/badge';
+import { Checkbox } from '@/components/ui/checkbox';
 import {
   Tooltip,
   TooltipContent,
